d8edd982f6a7166647cac7bc217a7328fba091f2
tom <qdtom@foxmail.com>
2016-07-21 12:32:43 +0800

    Initial commit

---
+++ app/src/main/java/barscanner/linuxgg/com/barscanner/MainActivity.java
@@ -0,0 +1,15 @@
+package barscanner.linuxgg.com.barscanner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.zxing.client.android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
